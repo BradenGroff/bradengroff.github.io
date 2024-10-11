cd4ee605152d87748db889b07f5e80a5b3c8f961
BradenGroff <bgroff412@gmail.com>
2024-10-11 12:54:04 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function downloadData() {
+  event.preventDefault() ;
+var startdate = document.getElementById('startdate").value;
+var end date = document.getElementById('startdate").value;
+alert(startdate);
+alert(enddate);
+}
