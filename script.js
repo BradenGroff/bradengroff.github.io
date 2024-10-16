5a5647ad698886e582d84855aeec777215c053e4
BradenGroff <bgroff412@gmail.com>
2024-10-16 12:33:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function downloadData() {
   event.preventDefault() ;
 var startdate = document.getElementById('startdate").value;
 var end date = document.getElementById('startdate").value;
-alert(startdate);
+console.log(startdate);
 alert(enddate);
 }
